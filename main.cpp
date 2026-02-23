3f06c428ffd8dad3b77cd4fdfa5d066086c25ba1
lomakim <lomakim@gmail.com>
2026-02-22 16:07:24 -0800

    Update

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// COMSC-210 | Lab 12 | Loma Kim
+/* This program has an hrsGaming array which holds hours spent 
+   playing video games per day for the last 30 days */
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    // DECLARATIONS
+    const int SIZE = 30;
+    array <double, SIZE> hrsGaming;
+
+    return 0;
+}
