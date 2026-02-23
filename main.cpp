fabce7f5b56758feef1538aa7028a251f158ebbd
lomakim <lomakim@gmail.com>
2026-02-22 16:36:14 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -4,23 +4,38 @@
 #include <iostream>
 #include <fstream>
 #include <array>
-#include <vector>
 #include <algorithm>
+#include <numeric>
 using namespace std;
 
 
 int main() {
     // DECLARATIONS
     const int SIZE = 30;
+    double target = 0.0;    // Used to search for values
+    
+
     array <double, SIZE> hrsGaming;
 
     ifstream fin;
     fin.open("file.txt");
 
+    // Banner
+    cout << "**** HOURS GAMING CALCULATOR (PAST 30 DAYS) ****" << endl << endl;
+
+    // Read hours in file into array and print
     for(int i = 0; i < SIZE; i++){
         fin >> hrsGaming[i];
-        cout << "Day " << i + 1 << ": " << hrsGaming[i] << endl;
+        cout << "Day " << i + 1 << ": " << hrsGaming[i] << "\t";
+        if ((i + 1) % 3 == 0)
+            cout << endl;
     }
+
+    //cout << "\nTotal Hours Played: " 
+    //     << accumulate(hrsGaming.begin(), hrsGaming.end(), 0) << endl;
+    cout << "Minimum hours played: "
+         << *min_element(hrsGaming.begin(), hrsGaming.end()) << endl;
+
 
     
     fin.close();
